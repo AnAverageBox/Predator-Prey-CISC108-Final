412f5d2e62a296e3a7e949463827ba8a6e21e4de
AnAverageBox <67079915+AnAverageBox@users.noreply.github.com>
2023-11-01 12:48:41 -0400

    Update Sheep&Wolfs.py

--- Sheep&Wolfs.py
+++ Sheep&Wolfs.py
@@ -1,3 +1,36 @@
+'''
+Instructions: copy the text below into a comment at the top of your Python file.
+Put an X into the [ ] boxes for each milestone you believe you have finished.
+
+## Simulator Features
+# Milestone 1
+[ ] Sheep exist
+[ ] Wolves exist
+[ ] Animals move
+[ ] Animals live
+[ ] Animals die
+# Milestone 2
+[ ] Smooth movement
+[ ] Wolves eat
+[ ] Animals reproduce
+# Milestone 3
+[ ] Grass exists
+[ ] Grass lives
+[ ] Sheep eat
+[ ] Grass grows
+[ ] Green grass
+[ ] Show stats
+# Extra Credit
+[ ] Stable Settings
+[ ] Fancy Stats
+[ ] Plot Stats
+[ ] Control Panel
+[ ] Hunters
+[ ] Evolution
+'''
+from designer import *
+from bakery import assert_equal
+from dataclasses import dataclass
 from designer import *
 from bakery import assert_equal
 from dataclasses import dataclass
